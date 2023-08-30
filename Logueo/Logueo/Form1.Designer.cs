0a42b46c618102ae27716653392d80c41609b73f
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-08-30 11:06:25 -0300

    Add files via upload

---
+++ Logueo/Logueo/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Logueo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            LNombre = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(150, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(172, 154);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // LNombre
+            // 
+            LNombre.AutoSize = true;
+            LNombre.Location = new Point(187, 180);
+            LNombre.Name = "LNombre";
+            LNombre.Size = new Size(92, 15);
+            LNombre.TabIndex = 1;
+            LNombre.Text = "Ingrese Nombre";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 361);
+            Controls.Add(LNombre);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Iniciar Sesión";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label LNombre;
+    }
+}
