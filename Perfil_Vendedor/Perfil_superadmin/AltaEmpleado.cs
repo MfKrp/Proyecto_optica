9a2c4cf15a174b79b3c7841713d40cf75a2a93d7
matias <matias@DESKTOP-3O1V6FN>
2023-09-19 13:07:28 +0100

    se agrego la base del formulario de alta de empleado

---
+++ Perfil_Vendedor/Perfil_superadmin/AltaEmpleado.cs
@@ -0,0 +1,10 @@
+namespace Perfil_superadmin
+{
+    public partial class AltaEmpleados : Form
+    {
+        public AltaEmpleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
